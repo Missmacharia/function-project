6eb7775149001aa139c6a7708237ef9b8a46da62
Missmacharia <yvonnemacharia30@gmail.com>
2022-10-14 13:11:19 +0300

    added new files

---
+++ function.js
@@ -0,0 +1,100 @@
+const DateOFBirth= (age =>{
+
+  return new Date().getFullYear() -age
+})
+
+console.log(DateOFBirth(2003));
+
+
+const sum= ((a,b)=>{
+
+  return a+b
+})
+
+console.log(sum(10,10));
+
+//decision 
+
+const decision =((x,y)=>{
+    if (x > y){
+      console.log('x is greater than y');
+    } else {
+      console.log('y is grater');
+    }
+    
+  })
+  console.log(decision(50,90));
+  
+  //area of a rectangle
+  
+  const area =((width,length)=>{
+  
+    return width*length
+  })
+  
+  
+  console.log(area(20,30));
+  
+//   //area of a circle
+  const areaCircle =((PIE,radius)=>{
+  
+    return PIE*radius*radius
+  })
+  console.log(areaCircle(3.14,14));
+  
+  
+  
+  const increament=(x=>{
+  
+    return x=10, x>-20,x++
+  })
+  console.log(increament(10))
+  
+  
+  
+  // const numbers =((num1,num2)=>{
+  //   if(num1===num2){
+  //     console.log('numbers equal');
+  //   } else if(num1 > num2){
+  //   console.log('num1 is greater');
+  //   } else {
+  //     console.log('num1 is greater');
+  //   }
+  // })
+  // console.log(numbers(30,20));
+  
+  
+
+
+  // // assignment function
+  //  const number =(a)
+
+
+
+  function number (n-1);{
+    if (n >100) return;
+
+  if (n/3 ===0 && n/5===0){
+
+    console.log('fizzbuzz');
+
+  }else if (n/3===0) {
+
+console.log('fizz')
+
+  }else if(n/5===0){
+
+    console.log('Buzz');
+
+  }else{
+    console.log('n');
+  }
+    fizzbuzz ++
+  }
+
+  
+
+  
+    
+  
+   
