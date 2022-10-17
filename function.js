c356f3506fef6db72f62678cd54041dbde7e7020
Missmacharia <99560116+Missmacharia@users.noreply.github.com>
2022-10-17 16:16:18 +0300

    made  changes to the file

--- function.js
+++ function.js
@@ -1,6 +1,6 @@
 
   function number (n){
-    if (n >100) return;
+    if (n < 100) return;
 
   if (n/3 ===0 && n/5===0){
 
@@ -15,10 +15,29 @@ console.log('fizz')
     console.log('Buzz');
 
   }else{
-    console.log('n');
+    console.log(n);
   }
-    fizzbuzz ++
+    number(n +1)
   }
+number()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
   
 
