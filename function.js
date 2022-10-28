2a395ec41ed8ae38f7bb47aa9b35f21b736c1f6a
Missmacharia <99560116+Missmacharia@users.noreply.github.com>
2022-10-28 09:58:36 +0300

    Update function.js

--- function.js
+++ function.js
@@ -2,15 +2,15 @@
   function number (n){
     if (n < 100) return;
 
-  if (n/3 ===0 && n/5===0){
+  if (n%3 ===0 && n%5===0){
 
     console.log('fizzbuzz');
 
-  }else if (n/3===0) {
+  }else if (n%3===0) {
 
 console.log('fizz')
 
-  }else if(n/5===0){
+  }else if(n%5===0){
 
     console.log('Buzz');
 
@@ -19,7 +19,7 @@ console.log('fizz')
   }
     number(n +1)
   }
-number()
+number(100)
     
     
     
